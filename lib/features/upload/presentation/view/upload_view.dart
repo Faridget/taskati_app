e75a0f828dd9e2d9277e7bb954a8bd943b5b2e40
Faridget <faridget@gmail.com>
2024-03-11 22:42:16 +0300

    22

--- lib/features/upload/presentation/view/upload_view.dart
+++ lib/features/upload/presentation/view/upload_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:taskati_app/core/utils/colors.dart';
 
 class UploadView extends StatefulWidget {
   const UploadView({super.key});
@@ -10,6 +11,24 @@ class UploadView extends StatefulWidget {
 class _UploadViewState extends State<UploadView> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return   Scaffold(
+
+      appBar:AppBar(
+        actions: [
+          TextButton(onPressed: (){}, child:  Text('Done',style: TextStyle(fontSize: 18,color: AppColors.primary,),)),
+        ],
+      ),
+      body: const Center(
+        child: Column(
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage(''),
+              
+               
+            )
+          ],
+        ),
+      ),
+    );
   }
 }

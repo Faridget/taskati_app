cb58564515d0b8d491e14ffc11e4c8597a894af3
Faridget <faridget@gmail.com>
2024-04-01 21:02:28 +0300

    3

--- lib/features/profile/presentation/view/profile_view.dart
+++ lib/features/profile/presentation/view/profile_view.dart
@@ -23,8 +23,9 @@ class _ProfileViewState extends State<ProfileView> {
   @override
   Widget build(BuildContext context) {
     final box = Hive.box('user');
-    var darkMode = box.get('darkMode');
+    var darkMode = box.get('darkMode')??false;
     return   Scaffold(
+      resizeToAvoidBottomInset : false,
       appBar: AppBar(
         foregroundColor: AppColors.primary,
         actions: [
@@ -41,95 +42,97 @@ class _ProfileViewState extends State<ProfileView> {
            ),
       ],),
       body: 
-      Center(
-      child:ValueListenableBuilder(
-        valueListenable:  Hive.box('user').listenable(),
-        builder: (BuildContext context, dynamic value, Widget? child) {
-          String path = AppLocalStorage.box.get('image')??'';
-           String name = AppLocalStorage.box.get('name');
-            return   Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Stack(
-                  alignment: Alignment.bottomRight,
-                  children: [
-                    CircleAvatar(
-                      radius: 70,
-                      backgroundImage:path.isNotEmpty? 
-                      FileImage(File(path)) as ImageProvider
-                      :const AssetImage('assets/images/user.png')),
-                      Positioned(
-                        bottom: 0,
-                        right: 0,
-                        child: InkWell(
-                          onTap: () {
-                            
-                          },
-                          child: CircleAvatar(
-                              backgroundColor: Theme.of(context).colorScheme.background,
-                              foregroundColor: AppColors.primary,
-                            child: const Icon(Icons.camera_alt_rounded)),
-                        ),
-                      )
-                  ],
-                ),
-                const Gap(25),
-                 Divider(
-                  color: AppColors.primary,
-                 ),
-                const Gap(25),
-                Row(children: [
-                  Text(name,
-                  style: getTitleStyle(context,color: AppColors.primary),),
-                  const Spacer(),
-                  InkWell(
-                          onTap: () {
-                            showImageDialog( context,onTapCamera:(){
-                                 uploadFromGalley();
-                          },);},
-                          child: CircleAvatar(
-                            radius: 19,
-                            backgroundColor:AppColors.primary,
+      SingleChildScrollView(
+        child: Center(
+        child:ValueListenableBuilder(
+          valueListenable:  Hive.box('user').listenable(),
+          builder: (BuildContext context, dynamic value, Widget? child) {
+            String path = AppLocalStorage.box.get('image')??'';
+             String name = AppLocalStorage.box.get('name');
+              return   Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Stack(
+                    alignment: Alignment.bottomRight,
+                    children: [
+                      CircleAvatar(
+                        radius: 70,
+                        backgroundImage:path.isNotEmpty? 
+                        FileImage(File(path)) as ImageProvider
+                        :const AssetImage('assets/images/user.png')),
+                        Positioned(
+                          bottom: 0,
+                          right: 0,
+                          child: InkWell(
+                            onTap: () {
+                              
+                            },
                             child: CircleAvatar(
-                              radius: 17,
                                 backgroundColor: Theme.of(context).colorScheme.background,
                                 foregroundColor: AppColors.primary,
-                              child: const Icon(Icons.edit)
-                              ),
+                              child: const Icon(Icons.camera_alt_rounded)),
                           ),
-                        ),
-                        GestureDetector(
-                          onTap: (){
-                            showImageDialog( context,onTapCamera:() async{
-                               await  uploadFromCamra().then((value){
-                                setState(() {
-                                  Navigator.of(context).pop();
-                                });
-                               }, onTapGallery:() async{
-                                 await uploadFromGalley().then((value){
-                                    setState(() {
-                                      Navigator.of(context).pop();
-                                    });
-                                 });
-                               }
-                               
-                               );
-
-                            });
-                          },
                         )
-
-                ],)
-              
-              ],
-                        ),
-            );
+                    ],
+                  ),
+                  const Gap(25),
+                   Divider(
+                    color: AppColors.primary,
+                   ),
+                  const Gap(25),
+                  Row(children: [
+                    Text(name,
+                    style: getTitleStyle(context,color: AppColors.primary),),
+                    const Spacer(),
+                    InkWell(
+                            onTap: () {
+                              showImageDialog( context,onTapCamera:(){
+                                   uploadFromGalley();
+                            },);},
+                            child: CircleAvatar(
+                              radius: 19,
+                              backgroundColor:AppColors.primary,
+                              child: CircleAvatar(
+                                radius: 17,
+                                  backgroundColor: Theme.of(context).colorScheme.background,
+                                  foregroundColor: AppColors.primary,
+                                child: const Icon(Icons.edit)
+                                ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: (){
+                              showImageDialog( context,onTapCamera:() async{
+                                 await  uploadFromCamra().then((value){
+                                  setState(() {
+                                    Navigator.of(context).pop();
+                                  });
+                                 }, onTapGallery:() async{
+                                   await uploadFromGalley().then((value){
+                                      setState(() {
+                                        Navigator.of(context).pop();
+                                      });
+                                   });
+                                 }
+                                 
+                                 );
         
-        },
+                              });
+                            },
+                          )
+        
+                  ],)
+                
+                ],
+                          ),
+              );
+          
+          },
+        )
+            ),
       )
-    )
     
    );
 }
